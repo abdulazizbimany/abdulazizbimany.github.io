923b032e8b38c78f11acfe45e35070460b24cc22
Abdulazizsoud <48648000+Abdulazizsoud@users.noreply.github.com>
2020-04-19 02:15:45 +0300

    Add files via upload

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,18 +1,43 @@
-// This is the "Offline copy of assets" service worker
+//This is the "Offline copy of pages" wervice worker
 
-const CACHE = "pwabuilder-offline";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
+//Install stage sets up the index page (home page) in the cahche and opens a new cache
+self.addEventListener('install', function(event) {
+    var indexPage = new Request('index.html');
+    event.waitUntil(
+      fetch(indexPage).then(function(response) {
+        return caches.open('pwabuilder-offline').then(function(cache) {
+          console.log('[PWA Builder] Cached index page during Install'+ response.url);
+          return cache.put(indexPage, response);
+        });
+    }));
+  });
+  
+  //If any fetch fails, it will look for the request in the cache and serve it from there first
+  self.addEventListener('fetch', function(event) {
+    var updateCache = function(request){
+      return caches.open('pwabuilder-offline').then(function (cache) {
+        return fetch(request).then(function (response) {
+          console.log('[PWA Builder] add page to offline'+response.url)
+          return cache.put(request, response);
+        });
+      });
+    };
+  
+    event.waitUntil(updateCache(event.request));
+  
+    event.respondWith(
+      fetch(event.request).catch(function(error) {
+        console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
+  
+        //Check to see if you have it in the cache
+        //Return response
+        //If not in the cache, then return error page
+        return caches.open('pwabuilder-offline').then(function (cache) {
+          return cache.match(event.request).then(function (matching) {
+            var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
+            return report
+          });
+        });
+      })
+    );
   })
-);
