fb576ed6f17296e9fa4db9fc3e807d150f63783d
Abdulaziz Bimany <48648000+Abdulazizsoud@users.noreply.github.com>
2021-12-15 02:39:55 +0300

    Add files via upload

---
+++ test/dist/js/script.js
@@ -0,0 +1,7 @@
+alert("hello");
+// eslint-disable-next-line no-console
+console.log("test");
+// eslint-disable-next-line no-console
+console.log("test");
+console.log("test");
+console.log("test");
