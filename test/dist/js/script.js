b8cfcc22bd419df1e08a5c940d1def72aae2e786
Abdulaziz Bimany <48648000+Abdulazizsoud@users.noreply.github.com>
2021-12-15 02:47:42 +0300

    Delete test directory

--- test/dist/js/script.js
+++
@@ -1,7 +0,0 @@
-alert("hello");
-// eslint-disable-next-line no-console
-console.log("test");
-// eslint-disable-next-line no-console
-console.log("test");
-console.log("test");
-console.log("test");
